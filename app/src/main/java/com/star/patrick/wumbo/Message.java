60ade418366e0431a137cebf7fffc94804598889
GiliamMeeussen <giliam.m27@gmail.com>
2016-10-15 18:19:21 -0400

    Added constructor to message. Created sender class.

--- app/src/main/java/com/star/patrick/wumbo/Message.java
+++ app/src/main/java/com/star/patrick/wumbo/Message.java
@@ -1,8 +1,26 @@
 package com.star.patrick.wumbo;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.UUID;
+
 /**
  * Created by Kevin Xiao on 2016-10-15.
  */
 
-public class Message {
+public class Message implements Serializable {
+    private String text;
+    private Sender sender;
+    private Timestamp sendTime;
+    private Timestamp recieveTime;
+    private Channel channel;
+    private UUID id;
+
+    public Message(String text, Sender sender, Timestamp sendTime, Channel channel) {
+        this.text = text;
+        this.sender = sender;
+        this.sendTime = sendTime;
+        this.channel = channel;
+        this.id = UUID.randomUUID();
+    }
 }
