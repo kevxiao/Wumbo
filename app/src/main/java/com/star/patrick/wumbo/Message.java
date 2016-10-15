d4cff372ba76c29a484c8ca3dfa761aeb26b15c8
GiliamMeeussen <giliam.m27@gmail.com>
2016-10-15 19:01:44 -0400

    Added constructor to message. Created sender class.

--- app/src/main/java/com/star/patrick/wumbo/Message.java
+++ app/src/main/java/com/star/patrick/wumbo/Message.java
@@ -9,18 +9,46 @@ import java.util.UUID;
  */
 
 public class Message implements Serializable {
+    private UUID id;
     private String text;
     private Sender sender;
     private Timestamp sendTime;
-    private Timestamp recieveTime;
-    private Channel channel;
-    private UUID id;
+    private Timestamp receiveTime;
+    private UUID channelId;
 
-    public Message(String text, Sender sender, Timestamp sendTime, Channel channel) {
+    public Message(String text, Sender sender, Timestamp sendTime, UUID channelId) {
         this.text = text;
         this.sender = sender;
         this.sendTime = sendTime;
-        this.channel = channel;
+        this.channelId = channelId;
         this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Sender getSender() {
+        return sender;
+    }
+
+    public Timestamp getSendTime() {
+        return sendTime;
+    }
+
+    public Timestamp getReceiveTime() {
+        return receiveTime;
+    }
+
+    public UUID getChannelId() {
+        return channelId;
+    }
+
+    public void setReceiveTime(Timestamp receiveTime) {
+        this.receiveTime = receiveTime;
     }
 }
--- app/src/main/java/com/star/patrick/wumbo/MessageList.java
+++ app/src/main/java/com/star/patrick/wumbo/MessageList.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public interface MessageList {
+    List<Message> getAllMessages();
     List<Message> getAllMessagesSince(Timestamp ts);
     void addMessage(Message msg);
 }
