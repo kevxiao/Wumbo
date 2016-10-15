35f315bff63dcdbd9e19fd1a202ea520c763fc4a
Jesse Jie <jesse_jie@hotmail.com>
2016-10-15 15:19:30 -0400

    PeerListListener

--- app/src/main/java/com/star/patrick/wumbo/MainActivity.java
+++ app/src/main/java/com/star/patrick/wumbo/MainActivity.java
@@ -3,12 +3,14 @@ package com.star.patrick.wumbo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private WifiP2pManager.Channel mChannel;
     private BroadcastReceiver mReceiver;
     private IntentFilter mIntentFilter;
+    public static final String TAG = "SE464";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
