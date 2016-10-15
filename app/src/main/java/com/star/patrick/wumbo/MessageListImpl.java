c943b0283058b6a5892e9482790f28ef62884cd5
GiliamMeeussen <giliam.m27@gmail.com>
2016-10-15 19:54:29 -0400

    Fleshed out MessageListImpl. Quite a garbage add since method, sorry guys.

--- app/src/main/java/com/star/patrick/wumbo/MessageListImpl.java
+++ app/src/main/java/com/star/patrick/wumbo/MessageListImpl.java
@@ -2,6 +2,9 @@ package com.star.patrick.wumbo;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MessageListImpl implements MessageList {
@@ -14,7 +17,27 @@ public class MessageListImpl implements MessageList {
 
     @Override
     public List<Message> getAllMessagesSince(Timestamp ts) {
-        return null;
+        Comparator<Message> receiveTimeComparator = new Comparator<Message>() {
+            @Override
+            public int compare(Message msg1, Message msg2) {
+                return msg1.getReceiveTime().compareTo(msg2.getReceiveTime());
+            }
+        };
+        Message key = new Message(null, null, null, null);
+        key.setReceiveTime(ts);
+        int index = Collections.binarySearch(messages, key, receiveTimeComparator);
+
+        if (index >= messages.size()) {
+            return new ArrayList<>();
+        }
+
+        while ( index < messages.size() &&
+                index > 1 &&
+                messages.get(index).getReceiveTime().equals(messages.get(index - 1))) {
+            index--;
+        }
+
+        return messages.subList(index, messages.size()-1);
     }
 
     @Override
