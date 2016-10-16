b85f27abd290c417275df1d5411b1f6d2453f349
David Su <dysu@uwaterloo.ca>
2016-10-15 20:04:12 -0400

    fixed typos

---
+++ app/src/main/java/com/star/patrick/wumbo/NetworkManagerImpl.java
@@ -0,0 +1,14 @@
+package com.star.patrick.wumbo;
+
+
+public class NetworkManagerImpl implements NetworkManager {
+    @Override
+    public void send(Message msg) {
+        
+    }
+
+    @Override
+    public void onReceive(Message msg) {
+
+    }
+}
