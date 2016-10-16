05c0a2bc50847df2c1e4d9dae33fdef5b120893d
Jesse Jie <jesse_jie@hotmail.com>
2016-10-16 16:48:25 -0400

    Handshake service

--- app/src/main/java/com/star/patrick/wumbo/wifidirect/HandshakeHandler.java
+++ app/src/main/java/com/star/patrick/wumbo/wifidirect/HandshakeHandler.java
@@ -1,9 +1,23 @@
 package com.star.patrick.wumbo.wifidirect;
 
 
-public class HandshakeHandler implements Runnable {
-    @Override
-    public void run() {
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
+import java.net.Socket;
+
+// Handler that receives messages from the thread
+public final class HandshakeHandler extends Thread {
+    private Socket socket;
+    Context context;
+    HandshakeHandler(Socket s, Context c){
+        context = c;
+        socket = s;
+    }
+    public void run(){
+        Log.d("Client's InetAddress", "" + socket.getInetAddress());
+        Intent intent = new Intent(context, HandshakeHandler.class);
+        intent.putExtra("ClientInetAddress", socket.getInetAddress());
     }
 }
