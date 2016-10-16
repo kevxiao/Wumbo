d3e2d0ec31227d6b95c53a45dbab719d766f0837
GiliamMeeussen <giliam.m27@gmail.com>
2016-10-15 21:51:46 -0400

    Was missing some files for the chat adapter and xml view. Took out some unncessary code.

---
+++ app/src/main/java/com/star/patrick/wumbo/ChatAdapter.java
@@ -0,0 +1,83 @@
+package com.star.patrick.wumbo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by giliam on 10/15/2016.
+ */
+
+public class ChatAdapter extends BaseAdapter {
+
+    private final List<Message> messages;
+    private Activity context;
+
+    public ChatAdapter(Activity context, List<Message> messages) {
+        this.context = context;
+        this.messages = messages;
+    }
+
+    @Override
+    public int getCount() {
+        return null != messages ? messages.size() : 0;
+    }
+
+    @Override
+    public Message getItem(int position) {
+        return null != messages ? messages.get(position) : null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        Message msg = getItem(position);
+        LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            convertView = vi.inflate(R.layout.message_view, null);
+            holder = createViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //boolean myMsg = msg.getIsme();//Just a dummy check
+        //to simulate whether it me or other sender
+        holder.txtMessage.setText(msg.getText());
+        holder.sender.setText(msg.getSender().getDisplayName());
+        holder.txtInfo.setText(msg.getReceiveTime().toString());
+
+        return convertView;
+    }
+
+    private ViewHolder createViewHolder(View v) {
+        ViewHolder holder = new ViewHolder();
+        holder.txtMessage = (TextView) v.findViewById(R.id.txtMessage);
+//        holder.content = (LinearLayout) v.findViewById(R.id.content);
+//        holder.contentWithBG = (LinearLayout) v.findViewById(R.id.contentWithBackground);
+        holder.txtInfo = (TextView) v.findViewById(R.id.txtInfo);
+        holder.sender = (TextView) v.findViewById(R.id.sender);
+        return holder;
+    }
+
+    private static class ViewHolder {
+        private TextView txtMessage;
+        private TextView txtInfo;
+        private TextView sender;
+//        private LinearLayout content;
+//        private LinearLayout contentWithBG;
+    }
+}
