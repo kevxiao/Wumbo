0310a2a04b376f6e6742a9b41626e83f603bac13
GiliamMeeussen <giliam.m27@gmail.com>
2016-11-19 23:39:38 -0500

    Added the channel add funcion. Once kevin figures out what secret key to create the encrypted message with should be good to actually send out the channel_invite messages.

--- app/src/main/java/com/star/patrick/wumbo/message/ChannelInvite.java
+++ app/src/main/java/com/star/patrick/wumbo/message/ChannelInvite.java
@@ -1,5 +1,7 @@
 package com.star.patrick.wumbo.message;
 
+import com.star.patrick.wumbo.User;
+
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -11,8 +13,8 @@ public class ChannelInvite implements MessageContent, Serializable {
     private MessageType type = MessageType.CHANNEL_INVITE;
     private Info content;
 
-    public ChannelInvite(UUID id, String name, String key) {
-        content = new Info(id, name, key);
+    public ChannelInvite(UUID id, String name, String key, User invitee) {
+        content = new Info(id, name, key, invitee);
     }
 
     @Override
@@ -29,11 +31,13 @@ public class ChannelInvite implements MessageContent, Serializable {
         private final UUID id;
         private final String name;
         private final String key;
+        private final User invitee;
 
-        public Info(UUID id, String name, String key) {
+        public Info(UUID id, String name, String key, User invitee) {
             this.id = id;
             this.name = name;
             this.key = key;
+            this.invitee = invitee;
         }
 
         public UUID getId() {
@@ -46,6 +50,10 @@ public class ChannelInvite implements MessageContent, Serializable {
 
         public String getKey() {
             return key;
+        }
+
+        public User getInvitee() {
+            return invitee;
         }
     }
 }
